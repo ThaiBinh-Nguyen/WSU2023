c1b0683d75b5b4d19a186b0fcac8f2cde1ea9a6c
Thai Binh Nguyen <tbng0725@gmail.com>
2023-09-16 08:46:30 -0700

    CS450-Project1-Updated

--- CS450/Matrix/matrix.c
+++ CS450/Matrix/matrix.c
@@ -3,74 +3,74 @@
 #include <string.h>
 #include <math.h>
 
-// Function to calculate the determinant of a matrix
-double calculateDeterminant(double **matrix, int size) {
-    double determinant = 1.0;
+// Function to calculate the Determinant of a Matrix
+double Find_Determinant(double **Matrix, int size) {
+    double Determinant = 1.0;
 
-    for (int currentRow = 0; currentRow < size; currentRow++) {
+    for (int Current_Row = 0; Current_Row < size; Current_Row++) {
         // Find the row with the maximum absolute value in the current column
-        int pivotRow = currentRow;
-        for (int nextRow = currentRow + 1; nextRow < size; nextRow++) {
-            if (fabs(matrix[nextRow][currentRow]) > fabs(matrix[pivotRow][currentRow])) {
-                pivotRow = nextRow;
+        int Find_Pivot = Current_Row;
+        for (int Next_Row = Current_Row + 1; Next_Row < size; Next_Row++) {
+            if (fabs(Matrix[Next_Row][Current_Row]) > fabs(Matrix[Find_Pivot][Current_Row])) {
+                Find_Pivot = Next_Row;
             }
         }
 
         // If the pivot row is not the current row, swap rows
-        if (pivotRow != currentRow) {
-            double *tempRow = matrix[currentRow];
-            matrix[currentRow] = matrix[pivotRow];
-            matrix[pivotRow] = tempRow;
-            determinant = -determinant; // Change determinant sign
+        if (Find_Pivot != Current_Row) {
+            double *Temp_Row = Matrix[Current_Row];
+            Matrix[Current_Row] = Matrix[Find_Pivot];
+            Matrix[Find_Pivot] = Temp_Row;
+            Determinant = Determinant * (-1); // Change Determinant sign
         }
 
         // Eliminate rows below (to keep elements below diagonal equal to 0)
-        for (int nextRow = currentRow + 1; nextRow < size; nextRow++) {
-            double factor = matrix[nextRow][currentRow] / matrix[currentRow][currentRow];
-            for (int col = currentRow; col < size; col++) {
-                matrix[nextRow][col] -= factor * matrix[currentRow][col];
+        for (int Next_Row = Current_Row + 1; Next_Row < size; Next_Row++) {
+            double Factor = Matrix[Next_Row][Current_Row] / Matrix[Current_Row][Current_Row];
+            for (int Column = Current_Row; Column < size; Column++) {
+                Matrix[Next_Row][Column] = Matrix[Next_Row][Column] - Factor * Matrix[Current_Row][Column];
             }
         }
 
-        // Multiply determinant by the element on the current diagonal
-        determinant *= matrix[currentRow][currentRow];
+        // Multiply Determinant by the element on the current diagonal
+        Determinant = Determinant * Matrix[Current_Row][Current_Row];
     }
 
-    return determinant;
+    return Determinant;
 }
 
 int main() {
-    int matrixSize;
-    double **matrix = NULL;
+    int Size_of_Matrix;
+    double **Matrix = NULL;
 
-    // Read the size of the first matrix
-    scanf("%d", &matrixSize);
+    // Read the size of the first Matrix
+    scanf("%d", &Size_of_Matrix);
 
     // Consume the trailing newline character
     getchar();
 
-    // Initialize the matrix
-    matrix = (double **)malloc(matrixSize * sizeof(double *));
-    for (int i = 0; i < matrixSize; i++) {
-        matrix[i] = (double *)malloc(matrixSize * sizeof(double));
+    // Initialize the Matrix
+    Matrix = (double **)malloc(Size_of_Matrix * sizeof(double *));
+    for (int i = 0; i < Size_of_Matrix; i++) {
+        Matrix[i] = (double *)malloc(Size_of_Matrix * sizeof(double));
     }
 
-    // Read data for the matrix
-    for (int i = 0; i < matrixSize; i++) {
-        for (int j = 0; j < matrixSize; j++) {
-            scanf("%lf", &matrix[i][j]);
+    // Read data for the Matrix
+    for (int i = 0; i < Size_of_Matrix; i++) {
+        for (int j = 0; j < Size_of_Matrix; j++) {
+            scanf("%lf", &Matrix[i][j]);
         }
     }
 
-    // Calculate and print the determinant of the matrix
-    double det = calculateDeterminant(matrix, matrixSize);
+    // Calculate and print the Determinant of the Matrix
+    double det = Find_Determinant(Matrix, Size_of_Matrix);
     printf("%.0lf\n", det);
 
     // Free the memory
-    for (int i = 0; i < matrixSize; i++) {
-        free(matrix[i]);
+    for (int i = 0; i < Size_of_Matrix; i++) {
+        free(Matrix[i]);
     }
-    free(matrix);
+    free(Matrix);
 
     return 0;
 }
