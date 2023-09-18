08ca032bb0d0cb27118265f16a5d735c39affa35
ThaiBinhNguyen92 <tbng0725@gmail.com>
2023-09-17 21:36:35 -0700

    CS442-Project2-Updated

--- CS442/Project2/matrix_template.js
+++ CS442/Project2/matrix_template.js
@@ -50,12 +50,11 @@ class Mat4 {
         const c = Math.cos(angle);
         const s = Math.sin(angle);
         return new Mat4([
-            c, -s, 0, 0,
-            s,  c, 0, 0,
+            c, s, 0, 0,
+            -s,  c, 0, 0,
             0,  0, 1, 0,
             0,  0, 0, 1
         ]);
-
     }
 
     /**
@@ -72,7 +71,7 @@ class Mat4 {
         return new Mat4([
             c, 0,  s, 0,
             0, 1,  0, 0,
-           -s, 0,  c, 0,
+            -s, 0,  c, 0,
             0, 0,  0, 1
         ]);
     }
@@ -90,8 +89,8 @@ class Mat4 {
         const s = Math.sin(angle);
         return new Mat4([
             1, 0,  0, 0,
-            0, c, -s, 0,
-            0, s,  c, 0,
+            0, c, s, 0,
+            0, -s,  c, 0,
             0, 0,  0, 1
         ]);
     }
