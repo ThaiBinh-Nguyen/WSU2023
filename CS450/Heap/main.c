f73e37b15183447a7688dad90033806d5421ecba
ThaiBinhNguyen92 <tbng0725@gmail.com>
2023-10-04 10:03:45 -0700

    CS450-Project2-Updated

--- CS450/Heap/main.c
+++ CS450/Heap/main.c
@@ -7,145 +7,147 @@
 
 #define NUM_SKILLS 5
 
+// Sorting in descending order
 int compare(const void *a, const void *b) {
     return *((int*)b) - *((int*)a);
 }
 
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = (low - 1);
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] >= pivot) {  // Thay đổi tại đây để sắp xếp theo thứ tự giảm dần
+// Returns the correct position of the pivot element
+int hoare_partition(int arr[], int low, int high) {
+    
+    // Choose the pivot as the first element
+    int pivot = arr[low];
+    int i = low - 1;
+    int j = high + 1;
+
+    while (1) {
+        do {
             i++;
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-    return (i + 1);
-}
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        } while (arr[i] > pivot);
+        do {
+            j--;
+        } while (arr[j] < pivot);
+        if (i >= j)
+            return j;
+
+        // Swap them
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
 
+// Sorts the array in-place in descending order
+void tail_recursive_quick_sort(int arr[], int low, int high) {
+    while (low < high) {
+        // Partition the array
+        int p = hoare_partition(arr, low, high);
+        if (p - low < high - p) {
+            tail_recursive_quick_sort(arr, low, p);
+            low = p + 1;
+        } else {
+            tail_recursive_quick_sort(arr, p + 1, high);
+            high = p;
+        }
+    }
+}
+
+
+
 int main(int argc, char *argv[]) {
-    struct timespec start, end;  // Declare timespec variables for timing
-    int *skills = NULL; // Khởi tạo mảng skills với NULL
-    int n = 0; // Số người chơi
+    int *Skill_Array = NULL; // Declare Skill_Array
+    int n = 0; // The number of player
 
     while (1) {
         int flag = 1;
 
-        // Mở rộng mảng để đọc thêm thông tin người chơi
-        skills = (int *)realloc(skills, (n + 1) * NUM_SKILLS * sizeof(int));
-        if(skills == NULL) {
-        fprintf(stderr, "Memory allocation failed.\n");
-        exit(1);
+        // Increse tje size of Skill_Array to store information of player
+        Skill_Array = (int *)realloc(Skill_Array, (n + 1) * NUM_SKILLS * sizeof(int));
+        if (Skill_Array == NULL) {
+            fprintf(stderr, "Memory allocation failed.\n");
+            exit(1);
         }
 
-        // Đọc thông tin kỹ năng
+        // Get information for skill of player
         for (int j = 0; j < NUM_SKILLS; ++j) {
-            if (scanf("%d", &skills[n * NUM_SKILLS + j]) != 1) {
+            if (scanf("%d", &Skill_Array[n * NUM_SKILLS + j]) != 1) {
                 flag = 0;
-                //printf("Break, không đọc thêm được.\n"); // Thêm dòng này
                 break;
             }
         }
 
-        // ...
-
+        // Stop the loop
         if (!flag) {
-            //printf("Thoát khỏi vòng lặp.\n"); // Thêm dòng này
             break;
         }
 
-        //printf("Đọc thông tin cho người chơi số %d.\n", n); // Thêm dòng này
         n++;
-
     }
     
-    // Khai báo và cấp phát mảng sorted
-    int *sorted = (int *)malloc(n * sizeof(int)); 
+    // Declare Sorted_array
+    int *Sorted_array = (int *)malloc(n * sizeof(int)); 
 
-    // Kiểm tra việc cấp phát bộ nhớ có thành công hay không
-    if (sorted == NULL) {
-        // Xử lý lỗi ở đây
+    // Check the error
+    if (Sorted_array == NULL) {
         exit(1);
     }
+    const char *Skill_of_Player[] = {"SKILL_BREAKDANCING", "SKILL_APICULTURE", "SKILL_BASKET", "SKILL_XBASKET", "SKILL_SWORD", "TOTAL_XP"};
 
-    if (strcmp(argv[1], "standard") == 0) {
-        const char *skill_names[] = {"SKILL_BREAKDANCING", "SKILL_APICULTURE", "SKILL_BASKET", "SKILL_XBASKET", "SKILL_SWORD", "TOTAL_XP"};
+    for (int Position_of_Skill = 0; Position_of_Skill <= NUM_SKILLS; ++Position_of_Skill) {
+        printf("%s\n", Skill_of_Player[Position_of_Skill]);
 
-        for (int skill = 0; skill <= NUM_SKILLS; ++skill) {
-            clock_gettime(CLOCK_MONOTONIC, &start);
-            printf("%Sine\n", skill_names[skill]);
-            
-            for (int i = 0; i < n; ++i) {
-                if (skill < NUM_SKILLS) {
-                    sorted[i] = skills[i * NUM_SKILLS + skill];
-                } else {
-                    sorted[i] = 0;
-                    for (int j = 0; j < NUM_SKILLS; ++j) {
-                        sorted[i] += skills[i * NUM_SKILLS + j];
-                    }
+        // Prepare data for sorting
+        for (int i = 0; i < n; ++i) {
+            if (Position_of_Skill < NUM_SKILLS) {
+                Sorted_array[i] = Skill_Array[i * NUM_SKILLS + Position_of_Skill];
+            } else {
+                Sorted_array[i] = 0;
+                for (int j = 0; j < NUM_SKILLS; ++j) {
+                    Sorted_array[i] += Skill_Array[i * NUM_SKILLS + j];
                 }
             }
+        }   
+        
+        struct timespec start, end;
+        long time_in_micros;
 
-            qsort(sorted, n, sizeof(int), compare);
+        //If the user chooses the standard program
+        if (strcmp(argv[1], "standard") == 0) {
+            clock_gettime(CLOCK_MONOTONIC, &start);
+            qsort(Sorted_array, n, sizeof(int), compare);
             clock_gettime(CLOCK_MONOTONIC, &end);
 
-            long time_in_micros = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
-            for (int i = 0; i < n; ++i) {
-                printf("%d\n", sorted[i]);
-            }
-            printf("time taken: %ld\n", time_in_micros);
+            time_in_micros = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
 
+            // Print Sorted_array values
+            for (int i = 0; i < n; ++i) {
+                printf("%d\n", Sorted_array[i]);
+            }
+            printf("time taken: %ld microseconds\n", time_in_micros);
             printf("\n");
+
         }
-    } else if (strcmp(argv[1], "custom") == 0) {
-        const char *skill_names[] = {"SKILL_BREAKDANCING", "SKILL_APICULTURE", "SKILL_BASKET", "SKILL_XBASKET", "SKILL_SWORD", "TOTAL_XP"};
-
-        for (int skill = 0; skill <= NUM_SKILLS; ++skill) {
+        //If the user chooses the custom program
+        else if (strcmp(argv[1], "custom") == 0) {
             clock_gettime(CLOCK_MONOTONIC, &start);
-            printf("%Sine\n", skill_names[skill]);
-            
-            for (int i = 0; i < n; ++i) {
-                if (skill < NUM_SKILLS) {
-                    sorted[i] = skills[i * NUM_SKILLS + skill];
-                } else {
-                    sorted[i] = 0;
-                    for (int j = 0; j < NUM_SKILLS; ++j) {
-                        sorted[i] += skills[i * NUM_SKILLS + j];
-                    }
-                }
-            }
-
-            // Gọi hàm myradixsort với tham số local_max để tối ưu hóa thuật toán
-            //myradixsort(sorted, n, local_max); // Chú ý rằng bạn cần chỉnh sửa hàm myradixsort để nó nhận thêm tham số này
-            quickSort(sorted, 0, n - 1);
+            tail_recursive_quick_sort(Sorted_array, 0, n - 1);
             clock_gettime(CLOCK_MONOTONIC, &end);
 
-            long time_in_micros = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
-            for (int i = 0; i < n; ++i) {
-                printf("%d\n", sorted[i]);
-            }
-            printf("time taken: %ld\n", time_in_micros);
+            time_in_micros = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
 
+            // Print Sorted_array values
+            for (int i = 0; i < n; ++i) {
+                printf("%d\n", Sorted_array[i]);
+            }
+            printf("time taken: %ld microseconds\n", time_in_micros);
             printf("\n");
         }
     }
+        
+    //Free memory
 
-    // Giải phóng bộ nhớ
-
-    free(sorted);
-    free(skills);
+    free(Sorted_array);
+    free(Skill_Array);
 
     return 0;
 }
