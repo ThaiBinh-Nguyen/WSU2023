1b8c30b04cb96ae8453cbc55f60b308b90600fb7
ThaiBinhNguyen92 <tbng0725@gmail.com>
2023-10-18 15:10:12 -0700

    CS442

--- CS442/Project6/mesh_template.js
+++ CS442/Project6/mesh_template.js
@@ -33,12 +33,6 @@ function create_and_load_vertex_buffer(gl, vertices, usage) {
 
     return vertexBuffer;
 }
-function set_vertex_attrib_to_buffer(gl, program, attrib_name, buffer, size, type, normalized, stride, offset) {
-    let attrib_location = gl.getAttribLocation(program, attrib_name);
-    gl.enableVertexAttribArray(attrib_location);
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.vertexAttribPointer(attrib_location, size, type, normalized, stride, offset);
-}
 
 class Mesh {
     /** 
@@ -67,9 +61,7 @@ class Mesh {
      */
 
     static box( gl, program, width, height, depth ) {
-
-        let vertices = [
-            // x, y, z, 
+        let verts = [
             -1, -1, -1,  // Vertex 0
             1, -1, -1, // Vertex 1
             1,  1, -1, // Vertex 2
@@ -80,18 +72,20 @@ class Mesh {
             -1,  1,  1  // Vertex 7
         ];
 
-
-        // Indices for the cube
-        let indices = [
-            0, 1, 2, 2, 3, 0,  // Front face
-            4, 5, 6, 6, 7, 4,  // Back face
-            4, 5, 1, 1, 0, 4,  // Bottom face
-            7, 6, 2, 2, 3, 7,  // Top face
-            4, 0, 3, 3, 7, 4,  // Left face
-            1, 5, 6, 6, 2, 1   // Right face
+        let indis = [
+            // clockwise winding
+            
+            0, 1, 2, 2, 3, 0, 
+            4, 0, 3, 3, 7, 4, 
+            5, 4, 7, 7, 6, 5, 
+            1, 5, 6, 6, 2, 1,
+            3, 2, 6, 6, 7, 3,
+            4, 5, 1, 1, 0, 4,
+            
         ];
 
-        return new Mesh( gl, program, vertices, indices );
+
+        return new Mesh( gl, program, verts, indis );
     }
 
 
@@ -106,63 +100,10 @@ class Mesh {
         gl.bindBuffer( gl.ARRAY_BUFFER, this.verts );
         gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indis );
 
-        set_vertex_attrib_to_buffer( 
-            gl, this.program, 
-            "coordinates", 
-            this.verts, 3, 
-            gl.FLOAT, false, VERTEX_STRIDE, 0 
-        );
-
-
-        set_vertex_attrib_to_buffer( 
-            gl, this.program, 
-            "color", 
-            this.verts, 4, 
-            gl.FLOAT, false, VERTEX_STRIDE, 12
-        );
+        let atr_coord = gl.getAttribLocation(shader_program, "coordinates");
+        gl.vertexAttribPointer(atr_coord, 3, gl.FLOAT, false, 12, 0); // 24 bytes per vertex
+        gl.enableVertexAttribArray(atr_coord);
 
         gl.drawElements( gl.TRIANGLES, this.n_indis, gl.UNSIGNED_SHORT, 0 );
-    }
-
-    /**
-     * Parse the given text as the body of an obj file.
-     * @param {WebGLRenderingContext} gl
-     * @param {WebGLProgram} program
-     * @param {string} text
-     */
-    static from_obj_text( gl, program, text ) {
-        // your code here
-        return new Mesh( /*your arguments here*/ );
-    }
-
-    /**
-     * Asynchronously load the obj file as a mesh.
-     * @param {WebGLRenderingContext} gl
-     * @param {string} file_name 
-     * @param {WebGLProgram} program
-     * @param {function} f the function to call and give mesh to when finished.
-     */
-    static from_obj_file( gl, file_name, program, f ) {
-        let request = new XMLHttpRequest();
-        
-        // the function that will be called when the file is being loaded
-        request.onreadystatechange = function() {
-            // console.log( request.readyState );
-
-            if( request.readyState != 4 ) { return; }
-            if( request.status != 200 ) { 
-                throw new Error( 'HTTP error when opening .obj file: ', request.statusText ); 
-            }
-
-            // now we know the file exists and is ready
-            let loaded_mesh = Mesh.from_obj_text( gl, program, request.responseText );
-
-            console.log( 'loaded ', file_name );
-            f( loaded_mesh );
-        };
-
-        
-        request.open( 'GET', file_name ); // initialize request. 
-        request.send();                   // execute request
     }
 }
